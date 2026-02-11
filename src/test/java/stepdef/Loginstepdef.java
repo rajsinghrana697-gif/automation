630ee52813e471e0cc576f13ba3a44e869f77704
RAJ SINGH RANA <rajsinghrana697@gmail.com>
2026-02-11 10:19:07 +0530

    automation

---
+++ src/test/java/stepdef/Loginstepdef.java
@@ -0,0 +1,44 @@
+package stepdef;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Loginstepdef {
+    @Given("^open the browser$")
+    public void openTheBrowser() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("open the browser");
+    }
+
+    @And("^enter thr url$")
+    public void enterThrUrl() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("enter the url");
+    }
+
+    @And("^enter the cred$")
+    public void enterTheCred() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("enter the cred");
+    }
+
+    @When("^user is clicking on login button$")
+    public void userIsClickingOnLoginButton() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("user is clicking on login button");
+    }
+
+    @Then("^user should be able to navigate to homepage$")
+    public void userShouldBeAbleToNavigateToHomepage() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("user should be able to navigate to homepage");
+    }
+
+    @And("^enter the cred \"([^\"]*)\" and \"([^\"]*)\"$")
+    public void enterTheCredAnd(String arg0, String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("enter the cred <args0> <args1>+ agrs0 + agrs1");
+    }
+}
---
+++ src/test/resources/login.feature
@@ -0,0 +1,24 @@
+Feature: login page
+  Scenario: validate user should be able to login with valid cred
+    Given open the browser
+    And enter thr url
+    And enter the cred
+    When user is clicking on login button
+    Then user should be able to navigate to homepage
+
+  Scenario: validate user should be able to login with valid cred
+    Given open the browser
+    And enter thr url
+    And enter the cred "<rajsinghrana>" and "<9622>"
+    When user is clicking on login button
+    Then user should be able to navigate to homepage
+
+  Scenario Outline: validate user should be able to login with valid cred
+    Given open the browser
+    And enter thr url
+    And enter the cred "<username>" and "<password>"
+    When user is clicking on login button
+    Then user should be able to navigate to homepage
+    Examples:
+      |username|password|
+      |rajsinghrana|9622|
